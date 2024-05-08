3f22c58930a29ae1355c45aa898cb5f6b71cf4b5
SevenEun <biikaaboo@gmail.com>
2024-05-08 10:59:07 +0800

    compared

--- hello.c
+++ hello.c
@@ -3,12 +3,12 @@
 
 int main(void) 
 {
-    // string answer = get_string("What's your name? ");
-    // printf("Hello, %s\n ", answer);
+    string answer = get_string("What's your name? ");
+    printf("Hello, %s\n ", answer);
 
-    char name[100]; // Assuming the name won't exceed 100 characters
-    printf("Enter your name: ");
-    scanf("%99s", name); // Read a string from the user, limiting to 99 characters to avoid buffer overflow
+    // char name[100]; // Assuming the name won't exceed 100 characters
+    // printf("Enter your name: ");
+    // scanf("%99s", name); // Read a string from the user, limiting to 99 characters to avoid buffer overflow
 
-    printf("Hello, %s\n", name); // Print the greeting
+    // printf("Hello, %s\n", name); // Print the greeting
 }
