09a324492e6153beaef97f502904951ca2bf53b9
SevenEun <biikaaboo@gmail.com>
2024-05-08 13:11:26 +0800

    Add

--- meow.c
+++ meow.c
@@ -1,11 +1,22 @@
 #include <stdio.h>
 
+void meow(void);
+
 int main(void)
 {
-    int i = 3;
-    while (i > 0)
+    for (int i = 0; i < 3; i++)
     {
-        printf("meow\n");
-        i--;
+        meow();
     }
+    // int i = 3;
+    // while (i > 0)
+    // {
+    //     printf("meow\n");
+    //     i--;
+    // }
+}
+
+void meow(void)
+{
+    printf("meow\n");
 }
