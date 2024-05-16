0b2a3cecf8633967f45170a6d432dbc25a4e05f7
SevenEun <biikaaboo@gmail.com>
2024-05-16 10:15:48 +0800

    declares a string initialized w "HI!" and print the first 3 characters

--- hi.c
+++ hi.c
@@ -1,16 +1,8 @@
+#include <cs50.h>
 #include <stdio.h>
 
 int main(void) 
 {
-    /// not the best approach but jus for demonstration sake.
-    char c1 = 'H';
-    char c2 = 'I';
-    char c3 = '!';
-
-    /// Three separate placeholders for the three characters.
-    // printf("%c%c%c\n", c1, c2, c3);
-
-    /// %i is used to print an integer and converts the char values to thier corresponding ASCII values.
-    printf("%i %i %i\n", c1, c2, c3);
-
+    string s = "HI!";
+    printf("%c%c%c\n", s[0], s[1], s[2]);
 }
