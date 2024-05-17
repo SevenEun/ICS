b8661ea83ef75e1024b184e39dc1be389a7eb3d9
SevenEun <biikaaboo@gmail.com>
2024-05-17 17:54:39 +0800

    toupper

--- uppercase.c
+++ uppercase.c
@@ -1,4 +1,5 @@
 #include <cs50.h>
+#include <ctype.h>
 #include <stdio.h>
 #include <string.h>
 
@@ -8,16 +9,7 @@ int main(void)
     printf("After:  ");
     for (int i = 0, n = strlen(s); i < n; i++)
     {
-        /// if lowercase - is greater than or equal to little 'a' and 'z'.
-        if (s[i] >= 'a' && s[i] <= 'z')
-        {
-            /// As the uppercase 'A' is 65 ASCII value, and lowercase 'a' is 97 ASCII value, we then can subtract 32 from the lowercase to get the uppercase.
-            printf("%c", s[i] - 32);
-        }
-        else
-        {
-            printf("%c", s[i]);
-        }
+        printf("%c", toupper(s[i]));
     }
     printf("\n");
 }
