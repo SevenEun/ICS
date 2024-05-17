fea7d58855cf46810e3d9a8d6f71b55d95dca5a7
SevenEun <biikaaboo@gmail.com>
2024-05-17 17:14:03 +0800

    Using of strlen

--- length.c
+++ length.c
@@ -1,22 +1,10 @@
 #include <cs50.h>
 #include <stdio.h>
-
-int string_length(string s);
+#include <string.h>
 
 int main(void)
 {
     string name = get_string("Name: ");
-
-    int length = string_length(name);
-    printf("%i\n", length);    
-}
-
-int string_length(string s)
-{
-    int n = 0;
-    while (s[n] != '\0')
-    {
-        n++;
-    }
-    return n;
+    int length = strlen(name);
+    printf("%i\n", length);
 }
