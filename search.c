242ae22696b8a10375a99ff6d17e8d4c613fba35
SevenEun <biikaaboo@gmail.com>
2024-05-20 13:37:27 +0800

    correcting line 14

--- search.c
+++ search.c
@@ -11,7 +11,7 @@ int main(void)
      for (int i = 0; i < 7; i++)
      {
         /// Check if the number is in the array
-        if (numbers[1] == n)
+        if (numbers[i] == n)
         {
             printf("Found\n");
             return 0;
