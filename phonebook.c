142df212674397b38e793dc63900309b2efc23d3
SevenEun <biikaaboo@gmail.com>
2024-05-20 23:30:59 +0800

    Placing person objects into the people array.

--- phonebook.c
+++ phonebook.c
@@ -2,18 +2,33 @@
 #include <stdio.h>
 #include <string.h>
 
+typedef struct{
+    string name;
+    string number;
+}
+person;
+
 int main(void)
 {
-    string names[] = {"Carter", "David", "John"};
-    string numbers[] = {"+1=617=495-1000", "+1=617-495-1000", "+1-949-468-2750"};
+    person people[3];
+
+    /// Setting the new piece of syntax, this person's name. The period, a literal dot operator that says go inside this person and access thier name field or name attribute.
+    people[0].name = "Carter";
+    people[0].number = "+1-617-495-1000";
+
+    people[1].name = "David";
+    people[1].number = "+1-617-495-1000";
+
+    people[2].name = "John";
+    people[2].number = "+1-949-468-2750";
 
     string name = get_string("Name: ");
     for (int i = 0; i <3; i++)
     {
-        if (strcmp(names[i], name) == 0)
+        if (strcmp(people[i].name, name) == 0)
         {
             /// Plug in that person corresponding number and not just 'Found' but also found this specific number.
-            printf("Found %s\n", numbers[i]);
+            printf("Found %s\n", people[i].number);
             return 0;
         }
     }
