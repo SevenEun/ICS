37a8e6a1d4901cbddf919b3430c6d9dc996b171d
SevenEun <biikaaboo@gmail.com>
2024-05-21 15:27:49 +0800

    Dereferencing the pointer and adding an offset to move to the next character.

--- addresses.c
+++ addresses.c
@@ -2,7 +2,8 @@
 
 int main(void)
 {
-    int n = 50;
-    int *p = &n;
-    printf("%p\n", *p);
+    char *s = "HI!";
+    printf("%c", *s);
+    printf("%c", *(s + 1));
+    printf("%c\n", *(s + 2));
 }
