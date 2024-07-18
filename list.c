e9d5538c60e16e9ccda78bd6c19ae6786148231d
SevenEun <biikaaboo@gmail.com>
2024-07-18 15:32:02 +0800

    Refactor program to create linked list from command-line args.

--- list.c
+++ list.c
@@ -1,36 +1,55 @@
+#include <cs50.h>
 #include <stdio.h>
 #include <stdlib.h>
 
 typedef struct node
-{ 
+{
     int number;
     struct node *next;
-} node;
+}
+node;
 
 int main(int argc, char *argv[])
 {
+    // Memory for numbers
     node *list = NULL;
 
+    // For each command-line argument
     for (int i = 1; i < argc; i++)
     {
+        // Convert arguement to int
         int number = atoi(argv[i]);
 
+        // Allocating node for number 
         node *n = malloc(sizeof(node));
         if (n == NULL)
         {
-            // Free memoty thus far
             return 1;
         }
         n->number = number;
-        n->next = list;
-        list = n;
-    }
+        n->next = NULL;
 
-    // Print whole list
-    node *ptr = list;
-    while (ptr != NULL)
-    {
-        printf("%i\n", ptr->number);
-        ptr = ptr->next;
+        // If list is empty
+        if (list == NULL)
+        {
+            // This node is the whole list
+            list = n;
+        } 
+
+        // If list has numbers already
+        else
+        {
+            // Iterate overnodes in list 
+            for (node *ptr = list; ptr != NULL; ptr = ptr->next)
+            {
+                // If at end of list
+                if (ptr->next == NULL)
+                {
+                    // Append node
+                    ptr->next = n;
+                    break;
+                }
+            }
+        }
     }
 }
