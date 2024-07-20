69b9b7b3cd9ec949df6fb8ed627620b866201d79
SevenEun <biikaaboo@gmail.com>
2024-07-20 14:10:41 +0800

    Refactor input handling with get_int function and add sum calculation

--- calculator.py
+++ calculator.py
@@ -1,5 +1,17 @@
-x = int(input("x: "))
-y = int(input("y: "))
+def get_int(prompt):
+    while True:
+        # Python will try to get an input from the user, will try to convert it to an integer.
+        try:
+            return int(input(prompt))
+        except ValueError:
+            pass
 
-z = x / y 
-print(f"{z:.50f}")
+
+def main():
+    x = get_int("x: ")
+    y = get_int("y: ")
+
+    print(x + y)
+
+
+main()
