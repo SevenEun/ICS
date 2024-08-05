2c1b3af988a908b4fb960ca1fca47bae45fd243d
SevenEun <biikaaboo@gmail.com>
2024-08-05 16:46:35 +0800

    Count and print occurrences of each favorite language from CSV

--- favorites.py
+++ favorites.py
@@ -2,17 +2,14 @@ import csv
 
 with open("favorites.csv", "r") as file:
     reader = csv.DictReader(file)
-    scratch, c, python = 0, 0, 0
+    counts = {}
 
     for row in reader:
-        favorite = row ["language"]
-        if favorite == "Scratch":
-            scratch += 1
-        elif favorite == "C":
-            c += 1
-        elif favorite == "Python":
-            python += 1
+        favorite = row["language"]
+        if favorite in counts:
+            counts[favorite] += 1
+        else:  
+            counts[favorite] = 1
 
-print(f"Scratch: {scratch}")
-print(f"C: {c}")
-print(f"Python: {python}")
+for favorite in counts:
+    print(f"{favorite}: {counts[favorite]}")
